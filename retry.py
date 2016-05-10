b145ed9901a6b64b0aea73169f622c4ddb65b382
Gaal Milan <gaal.milan@outlook.com>
2016-05-10 17:21:18 +0200

    Fixes and converting to Python 3

--- retry.py
+++ retry.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import subprocess
@@ -6,16 +6,17 @@ import sys
 import time
 
 def main():
-    returncode = 1
     if len(sys.argv) == 1:
-        print "Usage: ./futtato.py command [arguments]"
+        print("Usage: sys.argv[0] command [arguments]")
     else:
-        while returncode:
+        while True:
             command = subprocess.Popen(sys.argv[1:])
             command.wait()
             returncode = command.returncode
             if returncode:
                 time.sleep(1)
+            else:
+                break
   
 if __name__ == "__main__":
     main()
